15a2135747216c9f3079179089059d59cd498056
Raghu Kasavajhula <rkapps.gae@gmail.com>
2026-05-20 08:02:23 -0700

    rustic boot lib

--- crates/rustic-boot/src/lib.rs
+++ crates/rustic-boot/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod auth;
+pub mod boot;
+pub mod config;
+pub mod conversation;
+pub mod routes;
+pub mod storage;
